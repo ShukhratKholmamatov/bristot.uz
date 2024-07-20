b869d980d33a3be78d2b29d5d2b40ab90dc6835f
Shukhrat Kholmamatov <142324315+ShukhratKholmamatov@users.noreply.github.com>
2024-07-20 08:57:04 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+let menu = document.getElementById('menu');
+let cross = document.getElementById('cross');
+let navbar_m = document.getElementById('navbar_m')
+
+function appearNav() {
+    navbar_m.style.display = 'flex';
+}
+
+function disappearNav() {
+    navbar_m.style.display = "none";
+}
